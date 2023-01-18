021d04279fe06193ab3f59fa7cec742ca2fa101f
Savinder <savindersingh500@gmail.com>
2023-01-19 03:00:07 +0530

    JS Logic Done

---
+++ Random  Project/index.js
@@ -0,0 +1,50 @@
+
+const btn = document.getElementsByTagName("button")[0];
+let drumRoll = new Audio('./Audio/drum-roll.mp3');
+let images = document.getElementsByClassName('images');
+images = Array.from(images);
+console.log(images)
+
+function changeImages() {
+    const num1 = Math.ceil(Math.random() * 6)
+    const num2 = Math.ceil(Math.random() * 6)  // 0.0000000000000000000000001.. to 5.9999999999999999999999999
+    console.log(num1, num2);
+   images[0].style.backgroundImage = `url("./images/image-${num1}.jpg")`;
+   images[1].style.backgroundImage = `url("./images/image-${num2}.jpg")`;
+}
+
+
+
+
+
+
+let num = 9;
+function counter() {
+    let handShake = document.getElementById("hand-shake");
+    if (num < 0) {
+        handShake.innerHTML = `🤝`;
+        drumRoll.pause();
+        // location.reload();
+        return
+    } else {
+        handShake.innerHTML = num;
+        num = num - 1;
+        console.log(num)
+        drumRoll.play();
+        changeImages();
+
+    }
+}
+
+let n = 0;
+
+btn.addEventListener("click", () => {
+    n++;
+    if (n === 1) {
+        setInterval(counter, 900);
+        btn.innerText = "Double click to start again"
+    }else{
+        location.reload();
+        
+    }
+})
