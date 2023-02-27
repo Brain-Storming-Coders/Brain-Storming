461987a0c3cb7f86d92c3f3f7384276a6f8f4ca2
Riya Tiwari <riyatiwari727@gmail.com>
2023-02-27 13:27:44 +0530

    Inception

---
+++ React/Inception/App.js
@@ -0,0 +1,31 @@
+// const heading = document.createElement("h1")
+// heading.innerHTML = "We are brainstorming."
+// const root = document.getElementById('root')
+// root.appendChild(heading)
+
+/* <h1>We are brainstorming.</h1> */
+// const heading = React.createElement("h1", {
+//     id : "title",
+//     className : "title title1"
+// }, "We are brainstorming.")
+// const root = ReactDOM.createRoot(document.getElementById('root1'))
+// root1 is wrong path and therefore it doesn't get rendered.
+// root.render(heading)
+
+/* <div id="container">
+            <h1 id="title1">Heading One</h1>
+            <h1 id="title2">Heading Two</h1>
+        </div> */
+
+// const heading1 = React.createElement("h1", {
+//     id : "title1",
+// }, "Heading One.")
+// const heading2 = React.createElement("h2", {
+//     id : "title2",
+// }, "Heading Two.")
+// const container = React.createElement("div", {
+//     id: "container"
+// }, [heading1, heading2])
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(container)
